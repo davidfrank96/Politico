ecaa2572aa0ce0d00a28e30d48b9164695e5ad04
Frank David <43038045+davidfrank96@users.noreply.github.com>
2019-01-30 02:49:01 -0800

    Revert "Bg admin fix 163344095"

--- UI/main.js
+++
@@ -1,7 +0,0 @@
-const realFileBtn = document.getElementById("real-file");
-const customBtn = document.getElementById("custom-button");
-const customtext = document.getElementById("custom-text");
-
-customBtn.addEventListener("click", function() {
-  realFileBtn.click();
-});
