2c362da2141e7eba528cdc98674e044828c4b742
Frank David <43038045+davidfrank96@users.noreply.github.com>
2019-01-31 04:03:24 -0800

    Revert "this is my get all party routes"

--- server/src/routes/partyRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import party from "../models/party.js";
-import partyController from "../controllers/partyControllers";
-const partyRoutes = express.Router();
-
-partyRoutes.get("/parties", partyController.getAllParties);
-
-export default partyRoutes;
