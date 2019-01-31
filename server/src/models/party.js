b7187d70d47e3877aad06fb6919153c2cee8e190
David Frank <davidfrank96.df@gmail.com>
2019-01-31 03:46:33 -0800

    this is my get all party routes

---
+++ server/src/models/party.js
@@ -0,0 +1,28 @@
+const party = [
+  {
+    id: 101,
+    name: "Frank david",
+    hqAddress: "andela",
+    logoUrl: "www.jesusisy.com"
+  },
+  {
+    id: 102,
+    name: "victory",
+    hqAddress: "andela",
+    logoUrl: "www.jesusisy.com"
+  },
+  {
+    id: 103,
+    name: "malik",
+    hqAddress: "andela",
+    logoUrl: "www.jesusisy.com"
+  },
+  {
+    id: 104,
+    name: "tefe",
+    hqAddress: "andela",
+    logoUrl: "www.jesusisy.com"
+  }
+];
+
+export default party;
---
+++ server/src/routes/partyRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import party from "../models/party.js";
+import partyController from "../controllers/partyControllers";
+const partyRoutes = express.Router();
+
+partyRoutes.get("/parties", partyController.getAllParties);
+
+export default partyRoutes;
